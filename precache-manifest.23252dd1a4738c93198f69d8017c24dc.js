7c205d5c4abdc4a29771c37c87e08b84e24c04ec
unknown <sectumsempra82@gmail.com>
2019-04-29 00:19:16 +0200

    Updates

---
+++ precache-manifest.23252dd1a4738c93198f69d8017c24dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff917e5274f1ad6a4dc40ec13667c98",
+    "url": "/my-cv-react/index.html"
+  },
+  {
+    "revision": "5451888bcc27375d368f",
+    "url": "/my-cv-react/static/css/main.d8192758.chunk.css"
+  },
+  {
+    "revision": "18ef574c877ab611ded7",
+    "url": "/my-cv-react/static/js/2.4144d2fa.chunk.js"
+  },
+  {
+    "revision": "5451888bcc27375d368f",
+    "url": "/my-cv-react/static/js/main.bfcb70cc.chunk.js"
+  },
+  {
+    "revision": "26caa410a061c3c53846",
+    "url": "/my-cv-react/static/js/runtime~main.00205f95.js"
+  },
+  {
+    "revision": "b205fd78d2721888f18c8e3b6da7ac3f",
+    "url": "/my-cv-react/static/media/my-logo-full.b205fd78.jpg"
+  },
+  {
+    "revision": "404a80821a8584a9a91fb7a2bd49221e",
+    "url": "/my-cv-react/static/media/my-logo.404a8082.png"
+  }
+]);
