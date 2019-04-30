643709d0448977318bfd3ea018538626593af140
unknown <sectumsempra82@gmail.com>
2019-04-30 16:58:58 +0200

    Warning work in progress added

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,24 +4,28 @@ import Preface from '../../components/Homepage/Preface/Preface';
 import Skills from '../../components/Homepage/Skills/Skills';
 import RecentWork from '../../components/Homepage/RecentWork/RecentWork';
 import Modal from '../../components/UI/Modal/Modal';
+import monkey from "../../assets/images/monkey.gif";
 
 class Home extends Component {
 
-    
+
     state = {
         modal: false,
         sampleContent: ''
     }
 
+
+
     showSample = () => {
         this.setState({
-            modal: true
+            modal: true,
         })
     }
 
-    closeSample= () => {
+    closeSample = () => {
         this.setState({
-            modal: false
+            modal: false,
+            sampleContent: ''
         })
     }
 
@@ -31,17 +35,29 @@ class Home extends Component {
         })
     }
 
+    componentDidMount() {
+        const warningContent = (
+            <div style={{ textAlign: 'center', backgroundColor: 'yellow'}}>
+                <img src={monkey} alt='monkey' style={{margin: '0 auto'}}/><br/>
+                <h3>This website is still in construction, proceed at your own risk</h3><br/>
+                <button onClick={this.closeSample}>OK, PROCEED ANYWAY</button>
+            </div>
+            );
+        this.setSampleContent(warningContent);
+        this.showSample();
+    }
+
     render() {
         return (
             <React.Fragment>
                 <Modal show={this.state.modal} modalClosed={this.closeSample}>
                     {this.state.sampleContent}
                 </Modal>
-                    <Jumbo></Jumbo>
-                    <Preface></Preface>
-                    <Skills></Skills>
-                    <RecentWork showSample={this.showSample} setSampleContent={this.setSampleContent}></RecentWork>
-                
+                <Jumbo></Jumbo>
+                <Preface></Preface>
+                <Skills></Skills>
+                <RecentWork showSample={this.showSample} setSampleContent={this.setSampleContent}></RecentWork>
+
             </React.Fragment>
         )
     }
