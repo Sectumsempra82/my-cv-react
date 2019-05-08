e584ebcdda331103219de4130f837e758a438c62
unknown <sectumsempra82@gmail.com>
2019-05-08 19:32:45 +0200

    added Git links Signed-off-by: unknown <sectumsempra82@gmail.com>

--- src/components/Homepage/Demos/Demos.js
+++ src/components/Homepage/Demos/Demos.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import classes from './Demos.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Card, Button } from 'react-bootstrap';
 import demoBaz from '../../../assets/images/demoBaz.png'; 
 
@@ -10,6 +11,7 @@ class Demos extends Component {
         return (
             <section className={classes.Demos}>
                 <h1>DEMOS AND CODE SAMPLES</h1>
+                <h3>Find the code on <a href={'https://github.com/Sectumsempra82?tab=repositories'}>Git <FontAwesomeIcon icon={['fab', 'github']} /></a></h3>
                 <Card style={{ width: '18rem', margin: '0 auto' }}>
                     <Card.Img variant="top" src={demoBaz} />
                     <Card.Body>
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => (
-    <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" exact>Home</NavigationItem>
-    </ul>
+    <>
+        <ul className={classes.NavigationItems}>
+            <li><NavigationItem link="/" exact>Home</NavigationItem></li>
+            <li><NavigationItem link="https://github.com/Sectumsempra82?tab=repositories" exact><FontAwesomeIcon icon={['fab', 'github']} /></NavigationItem></li>
+        </ul>
+    </>
 );
 
 export default navigationItems;
